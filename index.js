1f9d58525007f91d819893f5418bb8f854c6befa
Gasper Krevs <gasper.krevs@gmail.com>
2023-05-14 03:14:09 +0200

    tak neki veliko gk

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
-let request = new XMLHttpRequest();
-request.open('GET', 'https://icanhazdadjoke.com/slack');
-request.send();
-request.onload = function() {
-    if (request.status === 200) {
-        let response = JSON.parse(request.response);
-        main_h1 = document.getElementById("main_h1");
-        main_h1.innerHTML = response.attachments[0].text;
-        console.log(response.attachments[0].text)
-    } else {
-        console.log(`error ${request.status} ${request.statusText}`);
-    }
-}
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
+
+  
